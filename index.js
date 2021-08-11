b751560e9e0dc6fbcdfdbacbdc2ed3119ee637ea
ShahNipun <shah.aa.nipun@gmail.com>
2021-08-11 06:07:04 +0600

    2nd problem

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ function seerToMon(seer) {
 
 const result = seerToMon(200);
 console.log(result);
+
+
+// problem-2
+
+function totalSales(shirt, pant, shoe) {
+
+    if (shirt >= 0 && pant >= 0 && shoe >= 0) {
+
+        const shirtCost = shirt * 100;
+        const pantCost = pant * 200;
+        const shoeCost = shoe * 500;
+
+        const totalprice = shirtCost + pantCost + shoeCost;
+
+        return totalprice;
+    }
+
+    else {
+        return "You enter a wrong amount";
+    }
+}
+const total = totalSales(2, 0, 3);
+console.log(total);
+
