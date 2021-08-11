ff674b991e2a3b02f30acf74bcbda6a532e44c37
ShahNipun <shah.aa.nipun@gmail.com>
2021-08-11 06:12:51 +0600

    3rd problem

--- index.js
+++ index.js
@@ -37,3 +37,46 @@ function totalSales(shirt, pant, shoe) {
 const total = totalSales(2, 0, 3);
 console.log(total);
 
+
+
+// problem-3
+
+function deliveryCost(n) {
+    const amount1 = 100;
+    const amount2 = 80;
+    const amount3 = 50;
+
+    if (n >= 0 && n <= 100) {
+        const shirt100 = n * amount1;
+
+        // const deliveryPrice = shirt100;
+        return shirt100;
+    }
+    else if (n >= 0 && n <= 200) {
+        const shirt100 = 100 * amount1;
+        const restshirt = n - 100;
+        const shirt80 = restshirt * amount2;
+
+        const deliveryPrice = shirt100 + shirt80;
+
+        return deliveryPrice;
+    }
+    else if (n > 200) {
+        const shirt100 = 100 * amount1;
+        const shirt80 = 100 * amount2;
+        const restshirt = n - 200;
+        const shirt50 = restshirt * amount3;
+
+        const deliveryPrice = shirt100 + shirt80 + shirt50;
+
+        return deliveryPrice;
+
+    }
+    else {
+        return "Sorry your input data is invalid";
+    }
+
+}
+const totalDelivery = deliveryCost(500);
+console.log(totalDelivery);
+
